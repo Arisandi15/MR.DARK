d98ad9c3c3d2ff3a94fad24449d78767ffdea1f6
Arisandi15 <60065358+Arisandi15@users.noreply.github.com>
2020-01-19 19:51:37 +0700

    Add files via upload

---
+++ MR.DARK.sh
@@ -0,0 +1,155 @@
+#usr/bin/bash
+read -p "username : " user
+if [ $user = "Sayang" ]
+then
+clear
+fi
+read -p "password : " pass
+if [ $pass = "Mantan" ]
+then
+clear
+toilet -f slant --gay "Login Success"
+sleep 5
+fi
+
+clear
+toilet -f standard "TOOL ARISANDI" -F gay
+sleep 2
+toilet -f standard "SUBSCRIBE" -F gay
+sleep 2
+toilet -f standard "Hrln.Arisandi" -F gay
+sleep 2
+echo "
+=============================
+AUTHOR  : Arisandi
+CHANNEL : Hrln.Arisandi
+TEAM    : HSD(HACKKERSCANDARK)
+CONTACT : 081336501311
+=============================" | lolcat
+
+sleep 2
+
+echo "
+(1) HACK WA
+(2) HACK FB
+(3) KIRIM VIRUS
+(4) SPAM SMS
+(5) RED_HAWK
+(6) DDOS
+(7) DDOS TROJANS
+(8) SPAM TROJANS
+(9) INSTALL 311 TOOLS
+(00) exit" | lolcat
+echo "pilih nomer : " | lolcat
+read nomer
+
+if [ $nomer = 1 ] || [ $nomer = 02 ]
+then
+clear
+pkg install git
+pkg install curl
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+fi
+
+if [ $nomer = 2 ] || [ $nomer = 02 ]
+then
+clear
+pkg install git
+pkg install mechanize
+pkg install python2
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+ls
+python2 SETAN.py
+fi
+
+if [ $nomer = 3 ] || [ $nomer = 03 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/justahackker/perusak
+cd perusak
+ls
+python2 perusak.py
+fi
+
+if [ $nomer = 4 ] || [ $nomer = 04 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+ls
+php telkomsel.php
+fi
+
+if [ $nomer = 5 ] || [ $nomer = 05 ]
+then
+clear
+pkg install python2
+pkg install curl
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php hawk.php
+fi
+
+if [ $nomer = 6 ] || [ $nomer = 06 ]
+then
+clear
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+chmod +x pukul.py
+python pukul.py
+fi
+
+if [ $nomer = 7 ] || [ $nomer = 07 ]
+then
+clear
+pkg install git
+pkg install python2 
+pkg install python3
+git clone https://github.com/mr-reo001-team/Trojan_MRREO
+cd Trojan_MRREO
+ls
+python3 jurusreo.py
+python2 jurusreo.py
+python jurusreo.py
+fi
+
+if [ $nomer = 8 ] || [ $nomer = 08 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/R133f/Trojans
+cd Trojans
+ls
+python2 trojans.py
+fi
+
+if [ $nomer = 9 ] || [ $nomer = 09 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+ls
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+if [ $nomer = 00 ] || [ $nomer = 00 ]
+then 
+clear
+echo "created by:ARISANDI" | lolcat
+exit
+fi
